d402df9f985c2a1aaf7e007cf98d3af0c23374e4
XCross01 <93390840+XCross01@users.noreply.github.com>
2022-06-13 01:21:24 -0400

    a

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -9,6 +9,6 @@ var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
 	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
 	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
-	router.HandleFunc("/book/{bookId}", controllers.UpdateBookId).Methods("PUT")
-	router.HandleFunc("/book/{bookId}", controllers.DeleteBookId).Methods("DELETE")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
 }
